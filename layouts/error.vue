7c6fa5fe4332573c8b6aada536a5d98e7f90b4d9
Wesley Matthews <81110199+wdmatthews@users.noreply.github.com>
2021-08-31 18:38:43 -0500

    Added an error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,5 +1,26 @@
 <template>
-  <span>Error {{ error.statusCode }}</span>
+  <v-row
+    no-gutters
+    justify="center"
+    align="center"
+    style="height: 100%"
+  >
+    <div class="text-center">
+      <v-img
+        src="/error.png"
+        class="mx-auto"
+        :max-width="256"
+      />
+      <h1 class="text-h6">
+        Error {{ error.statusCode }}
+      </h1>
+      <p class="mb-0">
+        Uh oh, it looks like an ingredient went missing!<br>
+        <!-- eslint-disable-next-line vue/singleline-html-element-content-newline -->
+        Try going to the <nuxt-link to="/menu">menu</nuxt-link>, I'm sure you can find something delicious there!
+      </p>
+    </div>
+  </v-row>
 </template>
 
 <script>
